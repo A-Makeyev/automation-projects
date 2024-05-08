ad78fb363014808cbe44be3d6f44ee98cac9e942
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-05-08 14:10:27 +0300

    update

--- Oxygen/Picanova/MerchOne/oxygen.env.js
+++ Oxygen/Picanova/MerchOne/oxygen.env.js
@@ -1,31 +1,29 @@
+module.exports = {
+    default: {
+        url: 'https://xxx:xxx@testing.merchone.com',
+        dashboardUrl: 'https://xxx:xxx@testing-dashboard.merchone.com',
+        supportUrl: 'https://support.merchone.com/en/support/home',
+        email: 'test.picanova.team@gmail.com', 
+        appPassword: 'xxxx xxxx xxxx xxxx',
+        password: 'xxxxxx'
+    },
 
-            module.exports = {
-                default: {
-                    url: 'https://merchone:karneval2@testing.merchone.com',
-                    dashboardUrl: 'https://merchone:karneval2@testing-dashboard.merchone.com',
-                    supportUrl: 'https://support.merchone.com/en/support/home',
-                    email: 'test.picanova.qa.team@gmail.com', 
-                    appPassword: 'anty lwwz jiyn dqgj', 
-                    password: 'cFsNiG355FsNiG'
-                },
+    TEST: { 
+        url: 'https://xxx:xxx@testing.merchone.com',
+        dashboardUrl: 'https://xxx:xxx@testing-dashboard.merchone.com',
+        supportUrl: 'https://support.merchone.com/en/support/home',
+        email: 'test.picanova.team@gmail.com', 
+        appPassword: 'xxxx xxxx xxxx xxxx', 
+        password: 'xxxxxx'
+    },
 
-                TEST: { 
-                    url: 'https://merchone:karneval2@testing.merchone.com',
-                    dashboardUrl: 'https://merchone:karneval2@testing-dashboard.merchone.com',
-                    supportUrl: 'https://support.merchone.com/en/support/home',
-                    email: 'test.picanova.qa.team@gmail.com', 
-                    appPassword: 'anty lwwz jiyn dqgj', 
-                    password: 'cFsNiG355FsNiG'
-                },
-
-                PROD: { 
-                    url: 'https://merchone.com',
-                    dashboardUrl: 'https://dashboard.merchone.com',
-                    supportUrl: 'https://support.merchone.com/en/support/home',
-					email: 'picanova.team@gmail.com', 
-                    appPassword: 'xxxx xxxx xxxx xxxx',
-                    password: 'xxxxxx'
-                },
-
-            }
+    PROD: { 
+        url: 'https://merchone.com',
+        dashboardUrl: 'https://dashboard.merchone.com',
+        supportUrl: 'https://support.merchone.com/en/support/home',
+        email: 'picanova.team@gmail.com', 
+        appPassword: 'xxxx xxxx xxxx xxxx',
+        password: 'xxxxxx'
+    },
+}
         
