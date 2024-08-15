f75cb091683252c93fbbba88cda187eb79df9cf4
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-08-15 14:36:02 +0300

    updated shlomo

--- Oxygen/Shlomo-Bit/Car Insurance/Compare Prices.js
+++ Oxygen/Shlomo-Bit/Car Insurance/Compare Prices.js
@@ -1,17 +1,19 @@
 // https://ioflood.com/blog/npm-xlsx
 
 web.transaction('Initialize Data')
+var file = 'השוואת פרופילי ביטוח פרטי - אתר משרד האוצר.xlsx'
+var fileCopy = file.split('.')[0] + ' - Copy.' + file.split('.')[1]
 var utils = require('./utils.js')
 var fs = require('fs')
 var XLSX = require('xlsx')
 var path = require('path')
 var dataFolder = path.join(__dirname, './Data')
 var resultsFolder = path.join(__dirname, './Results')
-var xlsxFilePath =  dataFolder + '\\חובה - אתר משרד האוצר.xlsx'
-var copy_xlsxFilePath = dataFolder + '\\חובה - אתר משרד האוצר - Copy.xlsx'
-var resultsFilePath = resultsFolder + `\\results ~ ${utils.currentDateTime()}_ID-${utils.randomNumber(6666, 9999)}.xlsx`
+var xlsxFilePath =  dataFolder + '\\' + file 
+var copy_xlsxFilePath = dataFolder + '\\' + fileCopy
+var resultsFilePath = resultsFolder + `\\results ~ ${utils.currentDateTime()}_ID-${utils.randomNumber(66666, 99999)}.xlsx`
 var workbook = XLSX.readFile(xlsxFilePath)
-var sheet = workbook.Sheets.sheet_1
+var sheet = workbook.Sheets.Everything
 
 web.transaction('Create A Copy Of The Data File')
 if (!fs.existsSync(copy_xlsxFilePath)) {
@@ -28,39 +30,44 @@ var resultsWorkbook = XLSX.utils.book_new()
 var companiesWritten = false
 var companies = []
 var ws_data = []
-var startRow = 39
-var lastRow = 41
+var startRow = 2
+var lastRow = utils.lastRowNumber(sheet)
 
 web.transaction('Open car.cma.gov.il')
 web.init()
 web.open('https://car.cma.gov.il')
+
+// if (web.isVisible('//div[@id="pagecontainer"]', 5000)) web.click('//input[@type="submit"]')
+// if (web.isVisible('//div[@class="m_txtErr"]', 5000)) assert.fail(web.getText('//div[@class="m_txtErr"]'))
 
 var windowWidth = web.execute(() => { return window.innerWidth })
 var windowHeight = web.execute(() => { return window.innerHeight })
 windowWidth > 1920 && web.setWindowSize(1700, windowHeight)
 
 try {
-    for (let profile = 1, row = startRow; row !== lastRow; row++, profile++) {
+    for (let profile = 1, row = startRow; row <= lastRow; row++, profile++) {
         var prices = []
         var displayData = []
 
         web.transaction(`Read Values ~ Row ${row}`)
-        var carType = utils.readFromCell(sheet, 'B', row) === 'פרטי' ? 'רכב פרטי' : utils.readFromCell(sheet, 'B', row)
-        var ownership = utils.readFromCell(sheet, 'C', row) === 'פרטית' ? 'בעלות פרטית' : 'בעלות אחרת'
-        var gender = utils.readFromCell(sheet, 'D', row) === 'גבר' ? '1' : '2'
-        var age = utils.readFromCell(sheet, 'E', row)
-        var seniority = utils.readFromCell(sheet, 'F', row)
-        var engineCapacity = utils.readFromCell(sheet, 'G', row)
-        var ABS = utils.readFromCell(sheet, 'H', row) === 'יש' ? 'קיימת' : 'לא קיימת'
-        var ESP = utils.readFromCell(sheet, 'I', row) === 'יש' ? 'קיימת' : 'לא קיימת'
-        var FCW = utils.readFromCell(sheet, 'J', row) === 'יש' ? 'קיימת' : 'לא קיימת'
-        var LDW = utils.readFromCell(sheet, 'K', row) === 'יש' ? 'קיימת' : 'לא קיימת'
-        var usage = utils.readFromCell(sheet, 'L', row)
-        var fuel = utils.readFromCell(sheet, 'M', row)
-        var insuranceDate = utils.readFromCell(sheet, 'N', row).split('.').join('/')
+        // var usage = utils.readFromCell(sheet, 'L', row)
+        // var carType = utils.readFromCell(sheet, 'B', row) === 'פרטי' ? 'רכב פרטי' : utils.readFromCell(sheet, 'B', row)
+        var ownership = utils.readFromCell(sheet, 'K', row) === 'בעלות פרטית' ? 'בעלות פרטית' : 'בעלות אחרת'
+        var gender = utils.readFromCell(sheet, 'C', row) === 'גבר' ? '1' : '2'
+        var age = utils.readFromCell(sheet, 'D', row)
+        var seniority = utils.readFromCell(sheet, 'E', row)
+        var engineCapacity = utils.readFromCell(sheet, 'A', row)
+        var ABS = utils.readFromCell(sheet, 'I', row) == 'קיימת' ? '1' : utils.readFromCell(sheet, 'I', row) == 'לא קיימת' ? '2' : ''
+        var ESP = utils.readFromCell(sheet, 'F', row) == 'קיימת' ? '1' : utils.readFromCell(sheet, 'F', row) == 'לא קיימת' ? '2' : ''
+        var FCW = utils.readFromCell(sheet, 'G', row) == 'קיימת' ? '1' : utils.readFromCell(sheet, 'G', row) == 'לא קיימת' ? '2' : ''
+        var LDW = utils.readFromCell(sheet, 'H', row) == 'קיימת' ? '1' : utils.readFromCell(sheet, 'H', row) == 'לא קיימת' ? '2' : ''
+        var fuel = utils.readFromCell(sheet, 'B', row)
+        var insuranceDate = utils.readFromCell(sheet, 'J', row) // .toString().split('.').join('/')
 
         web.transaction(`Enter Details ~ Row ${row}`)
-        web.select('id=ddlSheets', `label=${carType}`)
+        // web.select('id=ddlSheets', `label=${carType}`)
+        // web.select('id=B', `label=${usage}`)
+
         web.select('id=code_owner', `label=${ownership}`)
         web.type('id=insurance_date', insuranceDate)
         web.click(`//label[text()="מין הנהג"]//..//..//input[@value="${gender}"]`)
@@ -69,14 +76,17 @@ try {
         web.type('//input[contains(@placeholder, "הקלד מספר תאונות")]', '0')
         web.type('//input[contains(@placeholder, "הקלד מספר הרשעות")]', '0')
         web.select('id=N', `label=${fuel}`)
-        web.type('//input[contains(@placeholder, "הקלד נפח מנוע")]', engineCapacity)
+    
+        if (engineCapacity != '0') web.type('//input[contains(@placeholder, "הקלד נפח מנוע")]', engineCapacity)
+        
         web.type('//input[contains(@placeholder, "הקלד כוחות סוס")]', '100')
-        web.click(`//label[text()="מערכת למניעת נעילת גלגלים (ABS)"]//..//..//label[text()="${ABS}"]`)
-        web.click(`//label[text()="מערכת לבקרת יציבות (ESP)"]//..//..//label[text()="${ESP}"]`)
+        web.click(`//label[text()="מערכת למניעת נעילת גלגלים (ABS)"]//..//..//input[@value="${ABS}"]`)
+        web.click(`//label[text()="מערכת לבקרת יציבות (ESP)"]//..//..//input[@value="${ESP}"]`)
         web.type('//label[text()="מספר כריות אויר ברכב"]//..//..//input[@type="text"]', '4')
-        web.click(`//label[text()="מערכת התרעה על אי שמירת מרחק (FCW)"]//..//..//label[text()="${FCW}"]`)
-        web.click(`//label[text()="מערכת התרעה על סטיה מנתיב (LDW)"]//..//..//label[text()="${LDW}"]`)
-        web.select('id=B', `label=${usage}`)
+        web.click(`//label[text()="מערכת התרעה על אי שמירת מרחק (FCW)"]//..//..//input[@value="${FCW}"]`)
+        web.click(`//label[text()="מערכת התרעה על סטיה מנתיב (LDW)"]//..//..//input[@value="${LDW}"]`)
+        
+        web.transaction('Compare')
         web.click('id=press_to_compare')
 
         web.transaction(`Collect Results ~ Row ${row}`)
@@ -92,7 +102,7 @@ try {
                 companies.push(company) 
             }
 
-            companies.push('', '', 'תאריך ת. ביטוח', 'דלק', 'שימוש', 'LDW', 'FCW', 'ESP', 'ABS', 'נפח מנוע', 'ותק', 'גיל', 'מין', 'בעלות', 'סוג רכב')
+            companies.push('', '', 'בעלות', 'תאריך ת. ביטוח', 'ABS', 'LDW', 'FCW', 'ESP', 'ותק', 'גיל', 'מין', 'דלק', 'נפח מנוע')
             ws_data.push(companies)
             companiesWritten = true
         }
@@ -107,10 +117,15 @@ try {
         }
 
         gender = gender == '1' ? 'גבר' : 'אישה'
-        prices.push('', '', insuranceDate, fuel, usage, LDW, FCW, ESP, ABS, engineCapacity, seniority, age, gender, ownership, carType)
+        ABS = ABS == '1' ? 'קיימת' : ABS == '2' ? 'לא קיימת' : ''
+        LDW = LDW == '1' ? 'קיימת' : LDW == '2' ? 'לא קיימת' : ''
+        FCW = FCW == '1' ? 'קיימת' : FCW == '2' ? 'לא קיימת' : ''
+        ESP = ESP == '1' ? 'קיימת' : ESP == '2' ? 'לא קיימת' : ''
+
+        prices.push('', '', ownership, insuranceDate, ABS, LDW, FCW, ESP, seniority, age, gender, fuel, engineCapacity)
         ws_data.push(prices) 
 
-        utils.log('success', `Profile ${profile}:`)
+        utils.log('success', `Row ${row} Results:`)
         for (let x = 0; x < displayData.length; x++) {
             utils.log('info', displayData[x])
         }
