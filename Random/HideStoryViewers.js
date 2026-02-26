8ca1a962e2ab7ea1f2b9cbcaf111ed93b92bbcd1
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2026-02-26 16:57:45 +0200

    Update HideStoryViewers.js

--- Random/HideStoryViewers.js
+++ Random/HideStoryViewers.js
@@ -1,39 +1,30 @@
-var index = 1
-var tries = 200
-var hideStoryButton = '//div[@data-bloks-name="ig.components.Icon" and contains(@style, "rgb(219, 219, 219)") and not(contains(@style, "search"))]'
-
-var showStoryButton = '//div[@data-bloks-name="ig.components.Icon" and contains(@style, "rgb(0, 149, 246)") and not(contains(@style, "search"))]'
-
-
-while(index < tries) { 
-    document.evaluate(
-        `(${hideStoryButton})[${index}]`,
-        document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
-    ).singleNodeValue.click()
-	index++
-	tries--
+function sleep(ms) {
+    const end = Date.now() + ms
+    while (Date.now() < end) {}
 }
 
-while(index < tries) { 
-    document.evaluate(
-        `(${showStoryButton})[${index}]`,
-        document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
-    ).singleNodeValue.click()
-	index++
-	tries--
+function hideOrShowStory(action) {
+	var index = 1
+	var tries = 200
+	var hideStoryButton = '//div[contains(@style, "circle__outline")]'
+	var showStoryButton = '//div[contains(@style, "circle-check__filled")]'
+	
+	while(index <= tries) { 
+	    const result = document.evaluate(
+	        `(${action === 'hide' ? hideStoryButton : showStoryButton})[${index}]`,
+	        document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
+	    ).singleNodeValue
+
+		if (result !== null) {
+			result.scrollIntoView({ behavior: "smooth", block: "center" })
+			sleep(400)
+			result.click()
+			sleep(400)
+		}
+		
+		index++
+		tries--
+	}
 }
 
-
-const xpath = '//div[contains(@style, "circle__outline")]'
-// const xpath = '//div[contains(@style, "circle-check__filled")]'
-
-const result = document.evaluate(
-  xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
-)
-
-for (let i = 0; i < result.snapshotLength; i++) {
-  const el = result.snapshotItem(i)
-  if (el) {
-    el.click()
-  }
-}
+hideOrShowStory('hide')
