ab15ed4009cadf86071cfbde2e04f1e1dc6b8c54
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2026-02-28 18:12:31 +0200

    Update HideStoryViewers.js

--- Random/HideStoryViewers.js
+++ Random/HideStoryViewers.js
@@ -1,30 +1,23 @@
-function sleep(ms) {
-    const end = Date.now() + ms
-    while (Date.now() < end) {}
+function hideStoryFromEveryone(hide) {
+  const maxAttempts = 200
+  const hideStoryButtonXPath = '//div[contains(@style, "circle__outline")]'
+  const showStoryButtonXPath = '//div[contains(@style, "circle-check__filled")]'
+  
+  for (let index = 1; index <= maxAttempts; index++) {
+    const xpath = `(${hide ? showStoryButtonXPath : hideStoryButtonXPath})[${index}]`
+    const result = document.evaluate(
+      xpath,
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    ).singleNodeValue
+    
+    if (result !== null) {
+      result.scrollIntoView()
+      result.click()
+    }
+  }
 }
 
-function hideOrShowStory(action) {
-	var index = 1
-	var tries = 200
-	var hideStoryButton = '//div[contains(@style, "circle__outline")]'
-	var showStoryButton = '//div[contains(@style, "circle-check__filled")]'
-	
-	while(index <= tries) { 
-	    const result = document.evaluate(
-	        `(${action === 'hide' ? hideStoryButton : showStoryButton})[${index}]`,
-	        document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
-	    ).singleNodeValue
-
-		if (result !== null) {
-			result.scrollIntoView({ behavior: "smooth", block: "center" })
-			sleep(400)
-			result.click()
-			sleep(400)
-		}
-		
-		index++
-		tries--
-	}
-}
-
-hideOrShowStory('hide')
+hideStoryFromEveryone()
